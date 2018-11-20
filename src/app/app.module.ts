c1890609a8e52f8ec574fd781bb8b81f56669597
Philipp Kanter <philippkanter@gmail.com>
2018-11-20 18:13:19 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import {
@@ -22,13 +20,14 @@ import {
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { MyNavComponent } from './components/my-nav/my-nav.component';
 import { MyDashboardComponent } from './components/my-dashboard/my-dashboard.component';
 import { MyTableComponent } from './components/my-table/my-table.component';
-// import { FirstPageComponent } from './pages/first-page/first-page.component';
-// import { SecondPageComponent } from './pages/second-page/second-page.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { AnmeldungComponent } from './pages/anmeldung/anmeldung.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ZusagenComponent } from './pages/zusagen/zusagen.component';
 import { RsvpComponent } from './pages/zusagen/rsvp/rsvp.component';
 
@@ -38,8 +37,6 @@ import { RsvpComponent } from './pages/zusagen/rsvp/rsvp.component';
     MyNavComponent,
     MyDashboardComponent,
     MyTableComponent,
-    // FirstPageComponent,
-    // SecondPageComponent,
     AnmeldungComponent,
     ZusagenComponent,
     RsvpComponent
