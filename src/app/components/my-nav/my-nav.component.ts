fdf85802c9d9512020c56991f0369cd14924b9d8
Philipp <philippkanter@gmail.com>
2018-11-21 23:41:26 +0100

    menu fixed

--- src/app/components/my-nav/my-nav.component.ts
+++ src/app/components/my-nav/my-nav.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  AfterViewInit,
+  OnDestroy
+} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { FocusMonitor } from '@angular/cdk/a11y';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-my-nav',
   templateUrl: './my-nav.component.html',
-  styleUrls: ['./my-nav.component.scss'],
+  styleUrls: ['./my-nav.component.scss']
 })
-export class MyNavComponent {
+export class MyNavComponent implements AfterViewInit, OnDestroy {
+  mobileQuery: MediaQueryList;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+  private _mobileQueryListener: () => void;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private _focusMonitor: FocusMonitor,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
+  ngAfterViewInit() {
+    this._focusMonitor.stopMonitoring(document.getElementById('navButton_1'));
+  }
 }
