ea9a10bab417beea60f9f09e3feb913031f8ff9c
Philipp <philippkanter@gmail.com>
2018-11-23 08:56:01 +0100

    menu revert

--- src/app/components/my-nav/my-nav.component.ts
+++ src/app/components/my-nav/my-nav.component.ts
@@ -1,43 +1,28 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  AfterViewInit,
-  OnDestroy
-} from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { FocusMonitor } from '@angular/cdk/a11y';
-import { MediaMatcher } from '@angular/cdk/layout';
+// import { FocusMonitor } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-my-nav',
   templateUrl: './my-nav.component.html',
   styleUrls: ['./my-nav.component.scss']
 })
-export class MyNavComponent implements AfterViewInit, OnDestroy {
-  mobileQuery: MediaQueryList;
+// export class MyNavComponent implements AfterViewInit {
+export class MyNavComponent {
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(map(result => result.matches));
-  private _mobileQueryListener: () => void;
 
-  constructor(
-    private breakpointObserver: BreakpointObserver,
-    private _focusMonitor: FocusMonitor,
-    changeDetectorRef: ChangeDetectorRef,
-    media: MediaMatcher
-  ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
+  // constructor(
+  //   private breakpointObserver: BreakpointObserver,
+  //   private _focusMonitor: FocusMonitor
+  // ) {}
 
-  ngAfterViewInit() {
-    this._focusMonitor.stopMonitoring(document.getElementById('navButton_1'));
-  }
+  // ngAfterViewInit() {
+  //   this._focusMonitor.stopMonitoring(document.getElementById('navButton_1'));
+  // }
 }
