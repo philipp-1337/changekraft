9213dc58b4b822d06401db3f0736dd82ba4e813e
Philipp Kanter <philippkanter@MacBookPro152-philippkanter.local>
2020-02-20 18:57:07 +0100

    pwa not working (needs fix)

--- src/app/services/update.service.ts
+++ src/app/services/update.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-
-import { SnackbarClass } from 'src/app/shared/snackbar.class';
 import { SwUpdate } from '@angular/service-worker';
+import { SnackbarClass } from 'src/app/shared/snackbar.class';
 
 @Injectable()
 export class UpdateService {
-  constructor(private swUpdate: SwUpdate, private snackbar: SnackbarClass) {
-    this.swUpdate.available.subscribe(evt => {
-      const snack = this.snackbar.reloadSnackBar('Update Available', 'Reload');
+  constructor(swUpdate: SwUpdate, private snackbar: SnackbarClass) {
+    swUpdate.available.subscribe(event => {
+      this.snackbar.reloadSnackBar('Update Available:' + event.current, 'Neuladen');
     })
   }
 }
